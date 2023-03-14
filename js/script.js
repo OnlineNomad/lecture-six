0c1169dae4b7653ccefabea3e7dc8155a1cea14c
OnlineNomad <singularity123321@gmail.com>
2023-03-14 20:30:21 +0400

    Add task one, Fix task two

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
-// TASK TWO ------------------------------------------>
-function capitalizeFirst(string) {
-    const textArray = string.split(' ');
-    for (let i = 0; i < textArray.length; i++) {
-        textArray[i] = textArray[i][0].toUpperCase() + textArray[i].substring(1); 
-    }   
-    return textArray.join(' ');
+// TASK ONE ----------------------------------------->
+const replaceString = (string, toReplace, toReplaceWith) => {
+    let txtx = string.split(' ');
+    for(let i in txtx){
+        if(txtx[i] == toReplace){
+            txtx[i] = toReplaceWith;
+        }
+    }
+    return txtx.join(' ');
+
 }
 
+const stringOg = "Hello This is Nizami !";
+console.log(replaceString(stringOg, 'This', 'Thou'));
+
+
+// ----------------------------------------------|
+
+
+// TASK TWO ------------------------------------------>
+const capitalizeFirst = string => {
+    const textArray = string.split(' ');
+    return textArray.map((word) => {
+        return word[0].toUpperCase() + word.substring(1);
+    }).join(' ')
+}
 const text = "hi, my name is nizami uzunalovi";
 console.log(capitalizeFirst(text));
-
 // ------------------------------------------|
